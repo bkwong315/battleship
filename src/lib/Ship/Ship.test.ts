ee074065d00b629c141ed630877bc73d54b982aa
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-21 21:35:00 -0800

    Add getLocation and getLength methods and tests

--- src/lib/Ship/Ship.test.ts
+++ src/lib/Ship/Ship.test.ts
@@ -6,6 +6,29 @@ test('Ship is not sunk by default', () => {
   expect(carrier.isSunk()).toBe(false);
 });
 
+test('Ship length returns properly', () => {
+  const submarine = Ship(3);
+
+  expect(submarine.getLength()).toBe(3);
+});
+
+test('Ship location returns properly', () => {
+  const location = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ];
+  const cruiser = Ship(3, location);
+
+  const expected = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ];
+
+  expect(cruiser.getLocation()).toStrictEqual(expected);
+});
+
 test('Ship is not sunk when the number of hits is less than ship length', () => {
   const cruiser = Ship(3);
 
--- src/lib/Ship/Ship.ts
+++ src/lib/Ship/Ship.ts
@@ -1,11 +1,14 @@
-const Ship = (length: number) => {
-  const maxHp = length;
-  let hits = 0;
+const Ship = (len: number, loc: number[][] = []) => {
+  const length = len;
+  const location = loc;
+  let _hits = 0;
 
-  const hit = () => hits++;
-  const isSunk = () => maxHp - hits <= 0;
+  const hit = () => _hits++;
+  const isSunk = () => length - _hits <= 0;
+  const getLength = () => length;
+  const getLocation = () => location;
 
-  return { hit, isSunk };
+  return { hit, isSunk, getLength, getLocation };
 };
 
 export default Ship;
