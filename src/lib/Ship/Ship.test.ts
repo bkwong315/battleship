7da5adde156b18211effcb87646c949b8b6efe03
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-21 19:48:34 -0800

    Update ship tests

--- src/lib/Ship/Ship.test.ts
+++ src/lib/Ship/Ship.test.ts
@@ -6,8 +6,16 @@ test('Ship is not sunk by default', () => {
   expect(carrier.isSunk()).toBe(false);
 });
 
+test('Ship is not sunk when the number of hits is less than ship length', () => {
+  const cruiser = Ship(3);
+
+  cruiser.hit();
+
+  expect(cruiser.isSunk()).toBe(false);
+});
+
 test('Ship is sunk when the number of hits is equal to ship length', () => {
-  const destroyer = Ship(5);
+  const destroyer = Ship(2);
 
   while (!destroyer.isSunk()) {
     destroyer.hit();
