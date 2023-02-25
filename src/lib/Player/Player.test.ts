945e7d2b77c838f57eff310525dc4095c2bfaf20
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-24 17:47:05 -0800

    Change pid variable to name

--- src/lib/Player/Player.test.ts
+++ src/lib/Player/Player.test.ts
@@ -5,3 +5,11 @@ test('Test sendAttack method', () => {
 
   expect(p1.sendAttack([0, 0])).toStrictEqual({ sender: 'p1', coords: [0, 0] });
 });
+
+test('Player name set and get', () => {
+  const p1 = Player('p1');
+
+  p1.setName('New Name');
+
+  expect(p1.getName()).toBe('New Name');
+});
--- src/lib/Player/Player.ts
+++ src/lib/Player/Player.ts
@@ -1,20 +1,27 @@
 import GameBoard from '../GameBoard/GameBoard';
 
-const Player = (pid: string) => {
-  const playerId = pid;
+const Player = (initialName: string) => {
+  let name = initialName;
   const gameBoard = GameBoard();
+
+  const getName = () => name;
+  const setName = (newName: string) => (name = newName);
 
   const placeShip = (coords: [number, number], dir: string, shipType: string) =>
     gameBoard.placeShip(coords, dir, shipType);
 
   const sendAttack = (coords: [number, number]) => {
     return {
-      sender: playerId,
+      sender: name,
       coords,
     };
   };
 
-  return { placeShip, sendAttack };
+  const receiveAttack = (coords: number[]) => gameBoard.receiveAttack(coords);
+
+  const hasLost = () => gameBoard.isFleetDestroyed();
+
+  return { getName, setName, placeShip, sendAttack, receiveAttack, hasLost };
 };
 
 export default Player;
