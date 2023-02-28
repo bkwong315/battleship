02cf3f12bfa8466d368b8aafa6b13a83a6a28aa5
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-27 21:24:00 -0800

    Update App component to display board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
+import { useEffect, useState, useMemo, useRef } from 'react';
+import Game from './lib/Game/Game';
+import BoardDisplay from './components/BoardDisplay/BoardDisplay';
+import BoardData from './interfaces/boardData';
+
+import './App.scss';
+
 const App = () => {
+  const game: ReturnType<typeof Game> = useMemo(() => Game(), []);
+  const playerBoardData = useRef<BoardData>();
+  const computerBoardData = useRef<BoardData>();
+
+  const [computerAllShots, setComputerAllShots] = useState<number[][]>();
+  const [computerMissedShots, setComputerMissedShots] = useState<number[][]>();
+
+  useEffect(() => {
+    game.setPlayerName('John');
+
+    setComputerAllShots([]);
+    setComputerMissedShots([]);
+
+    playerBoardData.current = game.getPlayerBoard().getData();
+    computerBoardData.current = game.getComputerBoard().getData();
+
+    game.getComputerBoard().placeShip([0, 0], 'down', 'carrier');
+  }, []);
+
+  function updateBoard(coords: number[]) {
+    try {
+      game.playTurn(coords);
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+
+    if (computerBoardData.current !== undefined) {
+      setComputerAllShots([...computerBoardData.current.allShots]);
+      setComputerMissedShots([...computerBoardData.current.missedShots]);
+    }
+  }
+
+  useEffect(() => {
+    return;
+  }, [computerAllShots]);
+
   return (
     <>
-      <h1>Battleship!</h1>
+      <div className='layout'>
+        <div className='boards-container'>
+          {playerBoardData.current !== undefined && (
+            <BoardDisplay
+              boardData={{
+                allShots: playerBoardData.current.allShots,
+                missedShots: playerBoardData.current.missedShots,
+                ships: playerBoardData.current.ships,
+              }}
+            />
+          )}
+          {computerAllShots !== undefined &&
+            computerMissedShots !== undefined && (
+              <BoardDisplay
+                boardData={{
+                  allShots: computerAllShots,
+                  missedShots: computerMissedShots,
+                }}
+                cellCallback={updateBoard}
+              />
+            )}
+        </div>
+      </div>
     </>
   );
 };
