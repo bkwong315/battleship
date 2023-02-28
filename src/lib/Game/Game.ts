b40bc4335efd3b90c37ad00763cbd8f40ff46f8f
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-27 21:37:18 -0800

    Add computer turn logic

--- src/lib/Game/Game.ts
+++ src/lib/Game/Game.ts
@@ -12,6 +12,27 @@ const Game = () => {
 
   const playTurn = (coords: number[]) => {
     computer.receiveAttack(coords);
+    player.receiveAttack(getComputerAttack());
+  };
+
+  const getComputerAttack = () => {
+    const prevShots = getPlayerBoard().getData().allShots;
+    let targetCoords = [getRandomInt(10), getRandomInt(10)];
+
+    function getRandomInt(max: number) {
+      return Math.floor(Math.random() * max);
+    }
+
+    while (
+      prevShots.some(
+        (coords) =>
+          targetCoords[0] === coords[0] && targetCoords[1] === coords[1]
+      )
+    ) {
+      targetCoords = [getRandomInt(10), getRandomInt(10)];
+    }
+
+    return targetCoords;
   };
 
   return {
